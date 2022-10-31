91c37e5386d756fe4587da7b3f8509f4b085ab87
jmk5315 <31719854+jmk5315@users.noreply.github.com>
2022-10-31 15:57:41 +0900

    [Silver III] Title: N과 M (8), Time: 4 ms, Memory: 2064 KB -BaekjoonHub

---
+++ 백준/Silver/15657. N과 M （8）/N과 M （8）.cc
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <algorithm>
+#include <utility>
+#include <vector>
+#include <queue>
+#include <cstring>
+#include <deque>
+#include <cmath>
+
+
+
+using namespace std;
+typedef long long ll;
+
+int n,m;
+int visit[10005] ={0,};
+vector <int> arr;
+void foo(vector <int> v,int cnt){
+    if(cnt == m){
+        for(auto x : v){
+            cout << x << " ";
+        }
+        cout << "\n";
+        return;
+    }
+
+    for(int i = 0;i<n;i++){
+        if(!v.empty() && v[v.size()-1] > arr[i]){
+            continue;
+        }
+        v.push_back(arr[i]);
+        foo(v,cnt+1);
+        v.pop_back();
+    }
+}
+
+
+int main()
+{
+    cin.tie(NULL);
+    ios_base::sync_with_stdio(false);
+    cin >> n >> m;
+    for(int i = 0;i < n;i++){
+        int a;
+        cin >> a;
+        arr.push_back(a);
+    }
+    sort(arr.begin(),arr.end());
+    vector <int> arr2;
+    foo(arr2,0);
+    
+}
